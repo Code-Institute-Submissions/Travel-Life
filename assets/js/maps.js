82f313ffd1c78af0add514ccf2ac760c9a912028
Ubuntu <ubuntu@ip-172-31-46-159.ec2.internal>
2020-02-19 14:54:17 +0000

    finished adding css and amended the media queries for the elements and added clusterers in the map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,18 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 3.5,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 30.1534,
+            lng: 88.7879
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 27.5142, lng: 90.4336 },
+        { lat: 28.3949, lng: 84.1240 },
+        { lat: 30.1534, lng: 88.7879 }
     ];
 
     var markers = locations.map(function(location, i) {
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $(".mid-toggle").click(function(){
+     $(".toggle").show();   
+    });
+})
