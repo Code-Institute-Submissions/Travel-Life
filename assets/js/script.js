b7b51238edf97a349f1f23434d04b28c060ed1e2
Ubuntu <ubuntu@ip-172-31-46-159.ec2.internal>
2020-02-20 14:53:09 +0000

    added first script content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    $(".mid-toggle").click(function(){
-     $(".toggle").show();   
+$(document).ready(function() {
+    $(".firm").click(function() {
+        $(".img-text").hide("slow", '2000')
     });
-})
+$(".intro-title").mouseenter(function() {
+    $(this).next().children("img").addClass("bg-color")
+});
+    $(".btn-primary").click(function() {
+        $(this).next("p").toggle()
+    });
+    $(".img-text").mouseenter(function(){
+        $(this).addClass("bg-color")
+    });
+    $(".img-text").mouseleave(function(){
+        $(this).removeClass("bg-color")
+    });
+    
+});
