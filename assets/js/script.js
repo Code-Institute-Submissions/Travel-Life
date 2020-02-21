ff2abef1915e92fc15ee8ce685cff3686a5a8eee
Ubuntu <ubuntu@ip-172-31-46-159.ec2.internal>
2020-02-21 13:51:52 +0000

    added more javascript and fixed errors in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
     $(".firm").click(function() {
-        $(".img-text").hide("slow", '2000')
+        $("img").next().siblings().hide()
     });
-$(".intro-title").mouseenter(function() {
-    $(this).next().children("img").addClass("bg-color")
-});
+    $(".intro-title").mouseenter(function() {
+        $(this).next().addClass("bg-color")
+    });
+    $(".intro-title").mouseleave(function() {
+        $(this).next().removeClass("bg-color")
+    });
     $(".btn-primary").click(function() {
-        $(this).next("p").toggle()
+        $(this).next("p").toggle();
+        $(this).prev("img").toggle()
     });
-    $(".img-text").mouseenter(function(){
-        $(this).addClass("bg-color")
+    $(".img-text").mouseenter(function() {
+        $(this).children("a").addClass("bg-color").css("text-decoration", "none").css("font-size", "22px")
     });
-    $(".img-text").mouseleave(function(){
-        $(this).removeClass("bg-color")
+   
+    $(".img-text").mouseleave(function() {
+        $(this).children("a").removeClass("bg-color").css("font-size", "20px")
     });
-    
+
 });
